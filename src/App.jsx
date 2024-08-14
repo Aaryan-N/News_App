15d608ec54dbf3b9a9683f311b1dddee6768d8bb
Aaryan Narayan <aaryan.narayan@outlook.com>
2024-08-14 21:57:50 +1000

    Initial commit and setup up my news APP!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Components/Navbar.jsx'
+import NewsBoard from './Components/NewsBoard.jsx'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <NewsBoard/>
+        </div>
+    );
+};
+
+export default App;
